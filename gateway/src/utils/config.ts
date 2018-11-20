7ec42b5dabd9da3dabfd44a0bef67078fa4d9472
Collin Schaafsma <collin@collin.io>
2018-11-20 12:26:46 -0700

    adding basic logger and config utils

---
+++ gateway/src/utils/config.ts
@@ -0,0 +1,19 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export interface IConfig {
+    port: number;
+    tracing: boolean;
+    cacheControl: boolean;
+    appSecret: string;
+}
+
+const config: IConfig = {
+    port: +process.env.PORT || 4000,
+    tracing: !process.env.TRACING || true,
+    cacheControl: !process.env.TRACING || true,
+    appSecret: process.env.APPSECRET,
+};
+
+export { config };
